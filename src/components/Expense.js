eb6b46c91d93f379e6916b73e03c4445e4051c20
Sumit Saha <sumit87.dgp@gmail.com>
2022-05-04 13:16:29 +0530

    Refactored till lecture 39

---
+++ src/components/Expense.js
@@ -0,0 +1,5 @@
+function Expense(){
+
+}
+
+export default Expense;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 5, 4);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67;
+
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date ={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
