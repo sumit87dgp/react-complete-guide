7bfe4f72b9f63c781251fc9d79ea1a2579b2b5e6
Sumit Saha <sumit87.dgp@gmail.com>
2022-05-04 13:24:47 +0530

    Refactored further to add Expenseitem in Expense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expense";
 
 function App() {
   const expenses = [
@@ -27,10 +28,7 @@ function App() {
   return (
     <div className="App">
       <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expense expenses = {expenses} />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
