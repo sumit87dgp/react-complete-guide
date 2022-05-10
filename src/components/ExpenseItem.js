cc5b1c69d1f4df5229ad68bb2e43dba81ff2b68b
Sumit Saha <sumit87.dgp@gmail.com>
2022-05-10 15:35:36 +0530

    Card component issue resolved

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 5, 4);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67;
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date ={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
